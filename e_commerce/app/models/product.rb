2337c0ad514cf39600200e2bc6ec3291947f7246
Scott Braaten <sbraaten95@gmail.com>
2016-12-13 02:40:32 -0500

    renaming and organization complete

--- e_commerce/app/models/product.rb
+++
@@ -1,6 +0,0 @@
-class Product < ApplicationRecord
-  belongs_to :user
-  has_attached_file :photo
-  validates_attachment_presence :photo
-  validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
-end
